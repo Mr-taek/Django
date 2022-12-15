1c9386c47a5adaf41912310050e206cddd62ee9c
leekt <dlrmsxor9856@gmail.xom>
2022-12-15 18:30:15 +0900

    das

---
+++ mypj/vote/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from vote.models import LEEKT,voting
+# Register your models here.
+
+admin.site.register(LEEKT)
+admin.site.register(voting)
---
+++ mypj/vote/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+# 내가 여기서 모델을 추가하면 admin에서 뭔가 추가된다는거지??
+
+# table이름은.. LEEKT
+class LEEKT(models.Model):
+    nametext=models.CharField(max_length=100)#verbose_name="Building",name="constructure"
+    pub_date=models.DateField("data publising")
+
+    def __str__(self) -> str:
+        return self.nametext
+
+class voting(models.Model):
+    candidate=models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.candidate
